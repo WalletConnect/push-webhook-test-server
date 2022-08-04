3c0a912cad3d140d73348f75e7829c2838b98182
Derek <alexanderderekrein@gmail.com>
2022-08-04 16:15:17 +0200

    feat: deploys to Lambda

---
+++ terraform/backend.tf
@@ -0,0 +1,22 @@
+# Terraform Configuration
+terraform {
+  required_version = "~> 1.0"
+  required_providers {
+    assert = {
+      source = "bwoznicki/assert"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.9"
+    }
+  }
+
+  backend "s3" {
+    region               = "eu-central-1"
+    bucket               = "opz"
+    workspace_key_prefix = "infra/env"
+    key                  = "apps/push-webhook-test-server.tfstate"
+
+    force_path_style = true
+  }
+}
