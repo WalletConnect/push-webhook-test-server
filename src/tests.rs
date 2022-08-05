ba2e86505716a0197c2c7268d29d757c0989cd99
Derek <alexanderderekrein@gmail.com>
2022-08-05 13:45:41 +0200

    feat: validates input

--- src/tests.rs
+++ src/tests.rs
@@ -10,5 +10,17 @@ async fn test_lambda() {
   let response = function_handler(request).await.expect("failed to handle request");
 
   assert_eq!(response.status(), 200);
-  assert_eq!(response.into_body(), "{\"hello\": \"world\"}".into());
+  assert_eq!(response.into_body(), "{\"result\": \"posted result on SNS\"}".into());
+}
+
+#[tokio::test]
+async fn test_invalid_input() {
+  let input = include_str!("test_apigw_proxy_request_invalid_input.json");
+
+  let request = lambda_http::request::from_str(input)
+    .expect("failed to create request");
+
+  let response = function_handler(request).await.expect("failed to handle request");
+
+  assert_eq!(response.status(), 400);
 }
