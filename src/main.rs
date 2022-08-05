93877f512757856a74d85e22819e5a181ab8ced3
Derek <alexanderderekrein@gmail.com>
2022-08-05 15:02:17 +0200

    feat: validates input (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct TopicRequestBody {
+    topic: String,
+}
 
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/json")
-        .body("{\"hello\": \"world\"}".into())
-        .map_err(Box::new)?;
-    Ok(resp)
+    let invalid_payload_response = Response::builder()
+        .status(400)
+        .body("Invalid payload".into())
+        .expect("failed to render response");
+    if let Body::Text(body) = event.body() {
+        match serde_json::from_str::<TopicRequestBody>(&body) {
+            Ok(topic_body) => {
+                let _ = topic_body.topic;
+                let resp = Response::builder()
+                    .status(200)
+                    .header("content-type", "text/json")
+                    .body("{\"result\": \"posted result on SNS\"}".into())
+                    .map_err(Box::new)?;
+                Ok(resp)
+            }
+            Err(_) => Ok(invalid_payload_response)
+        }
+    } else {
+        Ok(invalid_payload_response)
+    }
 }
 
 #[tokio::main]
