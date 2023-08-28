5c21ae88011bae650964433088e8275cfcf45a59
Harry Bairstow <me@harryet.xyz>
2023-08-28 14:27:39 +0100

    feat: return payload to tests

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,104 @@
-use lambda_http::{run, service_fn, Body, Error, Request, Response};
-use aws_sdk_dynamodb::model::AttributeValue;
-use aws_sdk_dynamodb::{Client};
 use async_trait::async_trait;
-use tracing::{error, trace, info};
-use std::{env};
+use aws_sdk_dynamodb::model::AttributeValue;
+use aws_sdk_dynamodb::Client;
 use http::Method;
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
+use std::env;
 use std::time::{SystemTime, UNIX_EPOCH};
+use tracing::info;
 
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let config = aws_config::load_from_env().await;
     let ddb_client = aws_sdk_dynamodb::Client::new(&config);
 
-    let table_name = env::var("DDB_TABLE_NAME").expect("DDB_TABLE_NAME environment variable is not defined");
+    let table_name =
+        env::var("DDB_TABLE_NAME").expect("DDB_TABLE_NAME environment variable is not defined");
 
     match *event.method() {
         Method::POST => post_client_id(event, ddb_client, &table_name).await,
         Method::GET => get_client_id(event, ddb_client, &table_name).await,
-        _ => panic!("Method not supported")
+        _ => panic!("Method not supported"),
     }
 }
 
 #[async_trait]
 trait DynamoClient {
-    async fn put_client_id(&self, table: &str, client_id: &str) -> Result<(), Error>;
-    async fn get_client_id(&self, table: &str, client_id: &str) -> Result<(), Error>;
+    async fn put_client_id(&self, table: &str, client_id: &str, payload: &str)
+        -> Result<(), Error>;
+    async fn get_client_id(&self, table: &str, client_id: &str) -> Result<String, Error>;
 }
 
 #[async_trait]
 impl DynamoClient for aws_sdk_dynamodb::Client {
-    async fn put_client_id(&self, table: &str, client_id: &str) -> Result<(), Error> {
-        put_item(self, table, client_id).await
+    async fn put_client_id(
+        &self,
+        table: &str,
+        client_id: &str,
+        payload: &str,
+    ) -> Result<(), Error> {
+        put_item(self, table, client_id, payload).await
     }
-    async fn get_client_id(&self, table: &str, client_id: &str) -> Result<(), Error> {
+    async fn get_client_id(&self, table: &str, client_id: &str) -> Result<String, Error> {
         get_item(self, table, client_id).await
     }
 }
 
-async fn post_client_id(event: Request, ddb_client: impl DynamoClient, table_name: &str) -> Result<Response<Body>, Error> {
+async fn post_client_id(
+    event: Request,
+    ddb_client: impl DynamoClient,
+    table_name: &str,
+) -> Result<Response<Body>, Error> {
     let path = event.uri().path();
     let client_id = &path[9..path.len()];
-    info!("Posting record forc client_id: {}", client_id);
-    ddb_client.put_client_id(table_name, &client_id).await?;
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "text/json")
-        .body("{\"result\": \"posted result on DDB\"}".into())
-        .map_err(Box::new)?;
-    Ok(resp)
-}
-
-async fn get_client_id(event: Request, ddb_client: impl DynamoClient, table_name: &str) -> Result<Response<Body>, Error> {
-    let path = event.uri().path();
-    let client_id = &path[1..path.len()];
-    info!("Getting record forc client_id: {}", client_id);
-    match ddb_client.get_client_id(table_name, client_id.to_string().as_str()).await {
-        Ok(_) => {
+    let body = event.body();
+    match std::str::from_utf8(body) {
+        Ok(payload) => {
+            info!("Posting record for client_id: {}", client_id);
+            ddb_client
+                .put_client_id(table_name, &client_id, payload)
+                .await?;
             let resp = Response::builder()
                 .status(200)
                 .header("content-type", "text/json")
-                .body("{\"client_id\": \"exists\"}".into())
+                .body("{\"result\": \"posted result on DDB\"}".into())
                 .map_err(Box::new)?;
             Ok(resp)
         }
         Err(_) => Ok(Response::builder()
-        .status(404)
-        .header("content-type", "text/json")
-        .body("{\"client_id\": \"doesn't exist\"}".into())
-        .map_err(Box::new)?)
+            .status(400)
+            .header("content-type", "text/json")
+            .body("{\"result\": \"invalid body\"}".into())
+            .map_err(Box::new)?),
+    }
+}
+
+async fn get_client_id(
+    event: Request,
+    ddb_client: impl DynamoClient,
+    table_name: &str,
+) -> Result<Response<Body>, Error> {
+    let path = event.uri().path();
+    let client_id = &path[1..path.len()];
+    info!("Getting record for client_id: {}", client_id);
+    match ddb_client
+        .get_client_id(table_name, client_id.to_string().as_str())
+        .await
+    {
+        Ok(payload) => {
+            let body = format!("{{\"client_id\": \"exists\", \"payload\": {}}}", payload);
+
+            let resp = Response::builder()
+                .status(200)
+                .header("content-type", "text/json")
+                .body(body.into())
+                .map_err(Box::new)?;
+            Ok(resp)
+        }
+        Err(_) => Ok(Response::builder()
+            .status(404)
+            .header("content-type", "text/json")
+            .body("{\"client_id\": \"doesn't exist\"}".into())
+            .map_err(Box::new)?),
     }
 }
 
@@ -74,17 +106,20 @@ async fn put_item(
     client: &Client,
     table: &str,
     client_id: &str,
-  ) -> Result<(), Error> {
+    payload: &str,
+) -> Result<(), Error> {
     let sys_time = SystemTime::now();
     let since_the_epoch = sys_time.duration_since(UNIX_EPOCH).unwrap();
     let expiry_in_10_min = since_the_epoch.as_secs() + 600;
     let client_id_av = AttributeValue::S(client_id.into());
+    let payload_av = AttributeValue::S(payload.into());
     let exp_av = AttributeValue::N(expiry_in_10_min.to_string());
 
     let request = client
         .put_item()
         .table_name(table)
         .item("client_id", client_id_av)
+        .item("payload", payload_av)
         .item("expiry", exp_av);
 
     request.send().await?;
@@ -92,11 +127,7 @@ async fn put_item(
     Ok(())
 }
 
-async fn get_item(
-    client: &Client,
-    table: &str,
-    client_id: &str,
-  ) -> Result<(), Error> {
+async fn get_item(client: &Client, table: &str, client_id: &str) -> Result<String, Error> {
     let client_id_av = AttributeValue::S(client_id.into());
 
     let request = client
@@ -106,8 +137,12 @@ async fn get_item(
 
     let res = request.send().await?;
 
-    if res.item.is_some() {
-        Ok(())
+    if let Some(item) = res.item {
+        if let Some(payload) = item.get("payload") {
+            Ok(payload.as_s().unwrap().clone())
+        } else {
+            Err(Error::from("Missing payload"))
+        }
     } else {
         Err(Error::from("Item does not exist"))
     }
--- src/tests.rs
+++ src/tests.rs
@@ -1,67 +1,78 @@
 use lambda_http::Error;
 
-use crate::{post_client_id, get_client_id, DynamoClient};
+use crate::{get_client_id, post_client_id, DynamoClient};
 use async_trait::async_trait;
 
 struct MockDynamoDbClient {}
 
 #[async_trait]
 impl DynamoClient for MockDynamoDbClient {
-    async fn put_client_id(&self, _table: &str, client_id: &str) -> Result<(), Error> {
-      println!("client_id {}", client_id);
-      if client_id.eq("client_id-not-existing") {
-        Err(Error::from("Boom"))
-      } else {
-        Ok(())
-      }
+    async fn put_client_id(
+        &self,
+        _table: &str,
+        client_id: &str,
+        _payload: &str,
+    ) -> Result<(), Error> {
+        println!("client_id {}", client_id);
+        if client_id.eq("client_id-not-existing") {
+            Err(Error::from("Boom"))
+        } else {
+            Ok(())
+        }
     }
-    async fn get_client_id(&self, _table: &str, client_id: &str) -> Result<(), Error> {
-      println!("client_id {}", client_id);
-      if client_id.eq("client_id-not-existing") {
-        Err(Error::from("Boom"))
-      } else {
-        Ok(())
-      }
+    async fn get_client_id(&self, _table: &str, client_id: &str) -> Result<String, Error> {
+        println!("client_id {}", client_id);
+        if client_id.eq("client_id-not-existing") {
+            Err(Error::from("Boom"))
+        } else {
+            Ok(String::new())
+        }
     }
 }
 
 #[tokio::test]
 async fn test_post_nominal() {
-  let input = include_str!("test_apigw_proxy_request.json");
+    let input = include_str!("test_apigw_proxy_request.json");
 
-  let request = lambda_http::request::from_str(input)
-    .expect("failed to create request");
+    let request = lambda_http::request::from_str(input).expect("failed to create request");
 
-  let mock_ddb_client = MockDynamoDbClient{};
-  let response = post_client_id(request, mock_ddb_client, "test_table_name".into()).await.expect("failed to handle request");
+    let mock_ddb_client = MockDynamoDbClient {};
+    let response = post_client_id(request, mock_ddb_client, "test_table_name".into())
+        .await
+        .expect("failed to handle request");
 
-  assert_eq!(response.status(), 200);
-  assert_eq!(response.into_body(), "{\"result\": \"posted result on DDB\"}".into());
+    assert_eq!(response.status(), 200);
+    assert_eq!(
+        response.into_body(),
+        "{\"result\": \"posted result on DDB\"}".into()
+    );
 }
 
 #[tokio::test]
 async fn test_get_nominal() {
-  let input = include_str!("test_apigw_proxy_request.json");
+    let input = include_str!("test_apigw_proxy_request.json");
 
-  let request = lambda_http::request::from_str(input)
-    .expect("failed to create request");
+    let request = lambda_http::request::from_str(input).expect("failed to create request");
 
-  let mock_ddb_client = MockDynamoDbClient{};
-  let response = get_client_id(request, mock_ddb_client, "test_table_name".into()).await.expect("failed to handle request");
+    let mock_ddb_client = MockDynamoDbClient {};
+    let response = get_client_id(request, mock_ddb_client, "test_table_name".into())
+        .await
+        .expect("failed to handle request");
 
-  assert_eq!(response.status(), 200);
-  assert_eq!(response.into_body(), "{\"client_id\": \"exists\"}".into());
+    assert_eq!(response.status(), 200);
+    assert_eq!(response.into_body(), "{\"client_id\": \"exists\"}".into());
 }
 
 #[tokio::test]
 async fn test_get_client_id_does_not_exist() {
-  let input = include_str!("test_apigw_proxy_request_invalid_input.json");
+    let input = include_str!("test_apigw_proxy_request_invalid_input.json");
 
-  let request = lambda_http::request::from_str(input)
-    .expect("failed to create request");
+    let request = lambda_http::request::from_str(input).expect("failed to create request");
 
-  let mock_ddb_client = MockDynamoDbClient{};
-  let response = get_client_id(request, mock_ddb_client, "test_table_name".into()).await.expect("failed to handle request");
+    let mock_ddb_client = MockDynamoDbClient {};
+    let response = get_client_id(request, mock_ddb_client, "test_table_name".into())
+        .await
+        .expect("failed to handle request");
 
-  assert_eq!(response.status(), 404);
+    assert_eq!(response.status(), 404);
 }
